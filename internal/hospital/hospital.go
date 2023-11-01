1f9af6cfa848203877cb07ba7bd2082cf7ff6fd1
LysetsDal <asly@itu.dk>
2023-11-01 11:47:03 +0100

    Certs+Hospitalserver

--- internal/hospital/hospital.go
+++ internal/hospital/hospital.go
@@ -2,20 +2,41 @@ package hospital
 
 import (
 	"context"
+	"sync"
 
 	pb "github.com/LysetsDal/hospital_sec/proto"
+	"github.com/google/uuid"
+)
+
+const (
+	host = "localhost"
+	port = ":8081"
 )
 
 type IHospital interface {
-	SendToHospital(context.Context, *pb.HospitalRequest) (*pb.HospitalResponse) 
-    SendListToHospital(context.Context, *pb.HospitalListReq) (*pb.HospitalListRes) 
+	SendToHospital(context.Context, *pb.HospitalRequest) *pb.HospitalResponse
+	SendListToHospital(context.Context, *pb.HospitalListReq) *pb.HospitalListRes
 }
 
 type Hospital struct {
 	pb.HospitalServer
+	listenAddr string
+
+	connectionsMU sync.Mutex
+	connections   map[uuid.UUID]string
+
+	secretsArrayMU sync.Mutex
+	secretsArray   []int32
 }
 
-
 func NewServer() *Hospital {
-	return &Hospital{}
+	return &Hospital{
+		listenAddr:   host + port,
+		connections:  make(map[uuid.UUID]string),
+		secretsArray: make([]int32, 0),
+	}
+}
+
+func Start() error {
+	
 }
